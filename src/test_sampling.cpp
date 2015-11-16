a400d3505d37e3b1791dbf8fe61f632c24477afb
Jonas Maaskola <jonas.maaskola@scilifelab.se>
2015-11-16 17:03:30 +0100

    Check number of sampled events

--- src/test_sampling.cpp
+++ src/test_sampling.cpp
@@ -14,11 +14,28 @@ int main(int argc, char **argv) {
   vector<double> p = {0.5, 0.25, 0.25};
   EntropySource::seed();
   const size_t N = 1000;
-  for(size_t i = 0; i < N; ++i) {
-    auto x = sample_multinomial<size_t>(10000, p);
+  for (size_t i = 0; i < N; ++i) {
+    auto x = sample_multinomial<size_t>(N, p);
     auto y = sample_dirichlet<double>(x);
     print_vec(x);
     print_vec(y);
+
+    double sum = 0;
+    for (auto &z : x) sum += z;
+    if (sum != N) {
+      cout << "Error: the sum of multinomial counts " << sum
+           << " is not equal to " << N << endl;
+      exit(EXIT_FAILURE);
+    }
+
+    sum = 0;
+    for (auto &z : y) sum += z;
+    if (fabs(sum - 1.0) > 1e-6) {
+      cout << "Error: the sum of the dirichlet sampled distribution " << sum
+           << " is not equal to " << 1.0 << endl
+           << "Difference = " << (sum - 1.0) << endl;
+      exit(EXIT_FAILURE);
+    }
   }
 
   cout << endl;
