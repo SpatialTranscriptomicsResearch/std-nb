a059267ff27440710d0a133c1b184ae65244fc25
Jonas Maaskola <jonas.maaskola@scilifelab.se>
2015-11-24 12:08:37 +0100

    Specify output prefix

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <boost/filesystem.hpp>
 #include "cli.hpp"
 #include "PoissonFactorAnalysis.hpp"
 #include "io.hpp"
@@ -9,19 +10,22 @@
 using namespace std;
 using PFA = PoissonFactorAnalysis;
 
+const string default_output_string = "THIS PATH SHOULD NOT EXIST";
+
 struct Options {
   vector<string> paths;
   Verbosity verbosity = Verbosity::Verbose;
   size_t num_factors = 20;
   size_t num_steps = 2000;
   size_t report_interval = 20;
+  string output = default_output_string;
 };
 
-void write_resuls(const PFA &pfa) {
-  write_matrix(pfa.phi, "phi.txt");
-  write_matrix(pfa.theta, "theta.txt");
-  write_vector(pfa.r, "r.txt");
-  write_vector(pfa.p, "p.txt");
+void write_resuls(const PFA &pfa, const string &prefix) {
+  write_matrix(pfa.phi, prefix + "phi.txt");
+  write_matrix(pfa.theta, prefix + "theta.txt");
+  write_vector(pfa.r, prefix + "r.txt");
+  write_vector(pfa.p, prefix + "p.txt");
 }
 
 void perform_metropolis_hastings(const PFA::IMatrix &counts, PFA &pfa,
@@ -34,7 +38,7 @@ void perform_metropolis_hastings(const PFA::IMatrix &counts, PFA &pfa,
   double anneal = 1.0;
 
   auto res = mc.run(temperature, anneal, pfa, options.num_steps);
-  write_resuls(res.rbegin()->first);
+  write_resuls(res.rbegin()->first, options.output);
 }
 
 void perform_gibbs_sampling(const PFA::IMatrix &counts, PFA &pfa,
@@ -56,7 +60,7 @@ void perform_gibbs_sampling(const PFA::IMatrix &counts, PFA &pfa,
   }
   if (options.verbosity >= Verbosity::Info)
     cout << "Final log-likelihood = " << pfa.log_likelihood(counts) << endl;
-  write_resuls(pfa);
+  write_resuls(pfa, options.output);
 }
 
 int main(int argc, char **argv) {
@@ -93,7 +97,9 @@ int main(int argc, char **argv) {
     ("iter,i", po::value(&options.num_steps)->default_value(options.num_steps),
      "Number of iterations to perform.")
     ("report,r", po::value(&options.report_interval)->default_value(options.report_interval),
-     "Interval for computing and printing the likelihood.");
+     "Interval for computing and printing the likelihood.")
+    ("output,o", po::value(&options.output),
+     "Prefix for generated output files.");
 
   cli_options.add(generic_options).add(required_options).add(basic_options);
 
@@ -103,6 +109,14 @@ int main(int argc, char **argv) {
   ExecutionInformation exec_info = process_cli_options(
       argc, const_cast<const char **>(argv), options.verbosity, usage_info,
       cli_options, true, positional_options);
+
+  if(options.output == default_output_string) {
+    options.output = generate_random_label(exec_info.program_name, 0, options.verbosity) + "/";
+    if(not boost::filesystem::create_directory(options.output)) {
+      cout << "Error creating output directory " << options.output << endl;
+      exit(EXIT_FAILURE);
+    }
+  }
 
   auto counts = read_matrix(options.paths[0]);
 
