22f0c88bf4395173ae1a736b8f87a4ace2d66d15
Jonas Maaskola <jonas.maaskola@scilifelab.se>
2016-02-23 16:11:35 +0100

    Parse comma-separated list of mean forcing arguments

--- src/FactorAnalysis.cpp
+++ src/FactorAnalysis.cpp
@@ -11,24 +11,25 @@ Float digamma(Float x) { return boost::math::digamma(x); }
 Float trigamma(Float x) { return boost::math::trigamma(x); }
 
 istream &operator>>(istream &is, ForceMean &force) {
-  // TODO accept comma separated token lists
-  string token;
-  is >> token;
-  token = to_lower(token);
-  if (token == "theta")
-    force |= ForceMean::Theta;
-  else if (token == "phi")
-    force |= ForceMean::Phi;
-  else if (token == "spot")
-    force |= ForceMean::Spot;
-  else if (token == "experiment")
-    force |= ForceMean::Experiment;
-  else if (token == "default")
-    force |= ForceMean::Theta | ForceMean::Phi | ForceMean::Spot |
-             ForceMean::Experiment;
-  else
-    throw runtime_error("Error: could not parse mean forcing options'" + token +
-                        "'.");
+  while (is.good()) {
+    string token;
+    getline(is, token, ',');
+    token = to_lower(token);
+    if (token == "theta")
+      force |= ForceMean::Theta;
+    else if (token == "phi")
+      force |= ForceMean::Phi;
+    else if (token == "spot")
+      force |= ForceMean::Spot;
+    else if (token == "experiment")
+      force |= ForceMean::Experiment;
+    else if (token == "default")
+      force |= ForceMean::Theta | ForceMean::Phi | ForceMean::Spot |
+               ForceMean::Experiment;
+    else
+      throw runtime_error("Error: could not parse mean forcing options'" +
+                          token + "'.");
+  }
   return is;
 }
 
