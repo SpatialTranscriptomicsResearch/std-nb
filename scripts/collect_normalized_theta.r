86b0bba1d1dbc0ab0eb071df345b4a38bad08106
Jonas Maaskola <jonas.maaskola@scilifelab.se>
2016-05-20 03:17:32 +0200

    Collect the weighted theta matrices

--- scripts/collect_normalized_theta.r
+++ scripts/collect_normalized_theta.r
@@ -3,7 +3,7 @@
 source("~/code/multiScoopIBP/scripts/analyze.r")
 
 st.collect.normalized.thetas = function(dir) {
-  paths = list.files(dir, "iter.*_theta.txt")
+  paths = list.files(dir, "iter.*weighted_theta.txt")
   paths = sort(grep("_._theta", paths, invert=T, value=T))
 
   thetas = c()
@@ -11,10 +11,6 @@ st.collect.normalized.thetas = function(dir) {
     print(path)
 
     theta = st.load.matrix(dir, path)
-    phi = st.load.matrix(dir, gsub("theta", "phi", path))
-    scaling = st.load.vector(dir, gsub("theta", "spot_scaling", path))
-    theta = theta * scaling
-    theta = t(t(theta) * colSums(phi))
     theta = prop.table(theta, 1)
     thetas = cbind(thetas, theta)
   }
