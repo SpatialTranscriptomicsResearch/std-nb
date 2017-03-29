2ad4c4c5034c1cd3cd65d2dcf8675a7b61f17ceb
Jonas Maaskola <jonas.maaskola@scilifelab.se>
2017-03-29 21:18:46 +0200

    Fix copy-paste-fail-to-modify bug

--- src/priors.cpp
+++ src/priors.cpp
@@ -257,7 +257,7 @@ void Gamma::restore(const string &prefix) {
 
 void Gamma::enforce_positive_parameters(const string &tag) {
   enforce_positive_and_warn(tag + " r prior", r);
-  enforce_positive_and_warn(tag + " r prior", r);
+  enforce_positive_and_warn(tag + " p prior", p);
 }
 
 Dirichlet::Dirichlet(size_t dim1_, size_t dim2_, const Parameters &parameters)
