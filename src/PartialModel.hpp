86659b2250eee624ff54835b4467a7dcbd0f6f83
Jonas Maaskola <jonas.maaskola@scilifelab.se>
2017-03-30 06:22:41 +0200

    Remove unused member

--- src/PartialModel.hpp
+++ src/PartialModel.hpp
@@ -58,14 +58,13 @@ struct Model {
         dim2(dim2_),
         matrix(dim1, dim2),
         parameters(params),
-        prior(dim1, dim2, parameters),
-        alt_prior(dim1, dim2, parameters) {
+        prior(dim1, dim2, parameters) {
     initialize();
   };
   size_t dim1, dim2;
   Matrix matrix;
   Parameters parameters;
-  prior_type prior, alt_prior;
+  prior_type prior;
 
   void initialize_factor(size_t t);
   void initialize();
