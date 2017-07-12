a778c7d77143a4987119a8a1220ab701d8958f69
Jonas Maaskola <jonas.maaskola@scilifelab.se>
2017-07-12 16:53:32 +0200

    Clang-format

--- src/formula.cpp
+++ src/formula.cpp
@@ -1,18 +1,17 @@
+#include "formula.hpp"
 #include <sstream>
 #include "aux.hpp"
-#include "formula.hpp"
 
 using namespace std;
 
 Formula::Formula(const string &str) { from_string(str); }
 
 // TODO : Consider emitting syntactic error on space inside of covariates
-void Formula::from_string(const string &str)
-{
+void Formula::from_string(const string &str) {
   vector<string> terms = split_at('+', str);
-  for (string& term : terms) {
+  for (string &term : terms) {
     vector<string> covariates = split_at(':', term);
-    for(auto &covariate: covariates) {
+    for (auto &covariate : covariates) {
       covariate = trim(covariate);
     }
     formula.push_back(covariates);
@@ -22,8 +21,8 @@ void Formula::from_string(const string &str)
 string Formula::to_string() const {
   string str;
   bool first_term = true;
-  for (auto & term : formula) {
-    if(not first_term)
+  for (auto &term : formula) {
+    if (not first_term)
       str += "+";
     first_term = false;
     bool first_covariate = true;
