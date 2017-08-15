74b306099fc07420386d772cb6256094c76d8e05
Jonas Maaskola <jonas.maaskola@scilifelab.se>
2017-08-15 17:43:56 +0200

    Resurrect enrich program

--- src/enrich.cpp
+++ src/enrich.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include "compression.hpp"
+#include "counts.hpp"
 #include "entropy.hpp"
 #include "io.hpp"
 #include "log.hpp"
@@ -16,28 +17,35 @@ struct Data {
   vector<string> gene_names;
   vector<string> func_names;
   vector<string> type_names;
-  Matrix expression;
-  Matrix function;
+  Counts expression;
+  Counts function;
   size_t T, G, F;
   Data(const string &exp_path, const string &func_path)
-      : expression(parse_file<Matrix>(exp_path, read_floats, separator,
-                                      gene_names, type_names)),
-        function(parse_file<Matrix>(func_path, read_floats, separator,
-                                    gene_names, func_names)),
-        T(expression.n_cols),
-        G(expression.n_rows),
-        F(function.n_cols) {
+      : expression(exp_path, false),
+        function(func_path, false),
+        T(expression.matrix->cols()),
+        G(expression.matrix->rows()),
+        F(function.matrix->cols()) {
     // intersect gene_ids
+    vector<Counts> cnts;
+    cnts.push_back(expression);
+    cnts.push_back(function);
+    gene_intersection(cnts);
+
     // reduce to shared genes
+    expression = cnts[0];
+    function = cnts[1];
+
     // set T, G, and F
     LOG(info) << "T = " << T;
     LOG(info) << "G = " << G;
     LOG(info) << "F = " << F;
-    LOG(info) << "function.n_rows = " << function.n_rows;
+    LOG(info) << "function.n_rows = " << function.matrix->rows();
   }
+
   Matrix compute_expectation() const {
     LOG(info) << "Computing expectation";
-    Matrix m(T, F, arma::fill::zeros);
+    Matrix m = Matrix::Zero(T, F);
 #pragma omp parallel for if (DO_PARALLEL)
     for (size_t f = 0; f < F; ++f)
       for (size_t t = 0; t < T; ++t)
@@ -48,13 +56,13 @@ struct Data {
   pair<Matrix, Matrix> compute_shuffle_expectation(size_t N) const {
     // LOG(info) << "Computing expectation for shuffles";
     cerr << "Computing expectation for shuffles" << endl;
-    Matrix M(T, F, arma::fill::zeros);
-    Matrix Msd(T, F, arma::fill::zeros);
+    Matrix M = Matrix::Zero(T, F);
+    Matrix Msd = Matrix::Zero(T, F);
     vector<size_t> idx(G);
     for (size_t g = 0; g < G; ++g)
       idx[g] = g;
     for (size_t n = 0; n < N; ++n) {
-      Matrix m(T, F, arma::fill::zeros);
+      Matrix m = Matrix::Zero(T, F);
       if (n % 100 == 0)
         cerr << "Computing expectation for shuffle " << n << endl;
       shuffle(begin(idx), end(idx), EntropySource::rng);
@@ -77,14 +85,14 @@ struct Data {
 };
 
 ostream &print(ostream &os, const Matrix &m,
-           const vector<string> &row_names = vector<string>(),
-           const vector<string> &col_names = vector<string>()) {
+               const vector<string> &row_names = vector<string>(),
+               const vector<string> &col_names = vector<string>()) {
   for (auto name : col_names)
     os << "\t" << name;
   os << endl;
-  for (size_t i = 0; i < m.n_rows; ++i) {
+  for (int i = 0; i < m.rows(); ++i) {
     os << row_names[i];
-    for (size_t j = 0; j < m.n_cols; ++j)
+    for (int j = 0; j < m.cols(); ++j)
       os << "\t" << m(i, j);
     os << endl;
   }
@@ -104,8 +112,8 @@ int main(int argc, char **argv) {
   // LOG(info) << expected;
 
   print(cerr, observed, data.type_names, data.func_names);
-  print(cout, (observed - expected.first) / expected.second, data.type_names,
-        data.func_names);
+  print(cout, (observed - expected.first).array() / expected.second.array(),
+        data.type_names, data.func_names);
 
   return EXIT_SUCCESS;
 }
